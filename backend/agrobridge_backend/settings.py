11c478d4fdb697d413e77a5fdecb56d06ab5f476
Boateng Prince Agyenim <isbbydior@gmail.com>
2025-10-24 01:45:37 +0000

    Resolved Registration Issue

--- backend/agrobridge_backend/settings.py
+++ backend/agrobridge_backend/settings.py
@@ -68,7 +68,7 @@ REST_FRAMEWORK = {
         'authentication.authentication.CustomJWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
